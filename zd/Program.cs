d23546acdfb08357b44be6c507c93bea3812d105
Tanyasahapova2009 <tanyasahapova@mail.ru>
2022-12-19 22:33:55 +0300

    zd

---
+++ zd/Program.cs
@@ -0,0 +1,76 @@
+﻿Console.WriteLine("Выберите задание:");
+Console.WriteLine("Задача 1. Cумма натуральных элементов в промежутке от M до N.");
+Console.WriteLine("Задание 2. Программа вычисления функции Аккермана с помощью рекурсии.");
+
+int z = Convert.ToInt32(Console.ReadLine());
+switch (z)
+{
+    case 1:
+      Task1();
+      break;
+    case 2:
+    Task2();
+    break;
+          default:
+      break;
+}
+void Task1()
+{
+    Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+};
+
+void Task2()
+{Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+}
